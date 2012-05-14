1c888b8075ec31e664e3a913c904b814f402817f
Mark Lu <mlu@palantir.com>
2012-05-13 21:02:45 -0700

    finish ex2

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -6,6 +6,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = size(theta,1);
 
 % You need to return the following variables correctly 
 J = 0;
@@ -17,9 +18,36 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+summation = 0;
+for i = 1:m;
+	otx = sum(theta' .* X(i,:));
+	hx = 1 / (1 + (e^-otx));
+	loopsum = (-y(i) * log (hx)) - ((1-y(i)) * log((1-hx)));
+	summation = summation + loopsum;
+end;
+J = summation / m;
 
+summation = 0;
+for i = 2:n;
+	summation = summation + (theta(i)^2);
+end;
+J = J + ((lambda*summation)/(2*m));
 
+for h = 1: n;
+	summation = 0;
+	for i = 1:m;
+		otx = sum(theta' .* X(i,:));
+		hx = 1 / (1 + (e^-otx));
+		loopsum = (hx - y(i)) * X(i,h);
+		summation = summation + loopsum;
+	end;
+	
+grad(h) = summation / m;
 
+if (h >1)
+grad(h) = grad(h) + ((lambda/m)*theta(h));
+endif;
+end;
 
 
 % =============================================================
